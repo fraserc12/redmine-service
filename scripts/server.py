9f7ffd132ad8d37414bd4a41327e50f2d75902a6
fraserc12 <fraser.cassidy@hbr.org>
2020-08-13 21:30:35 +0100

    add endpoint for projects

--- scripts/server.py
+++ scripts/server.py
@@ -9,13 +9,17 @@ from get_hours_logged import *
 app = Flask(__name__, template_folder="templates")
 
 # Create a URL route in our application for "/"
-@app.route('/')
-def home():
-  # all_projects = fetch_project_details()
-  # project_selected = select_project(all_projects)
+@app.route('/time-logged')
+def time_logged():
   entries = fetch_time_entries('573')
   hour_summary = construct_hour_summary(entries)
   return dumps(hour_summary)
+
+# Create a URL route in our application for "/"
+@app.route('/projects')
+def projects():
+  all_projects = fetch_project_details()
+  return dumps(all_projects)
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
