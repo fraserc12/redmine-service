6fbd1e369097bd0f7208b755c74604fff6c9037b
fraserc12 <fraser.cassidy@hbr.org>
2020-08-14 10:49:41 +0100

    tidy up of routes

--- scripts/server.py
+++ scripts/server.py
@@ -1,25 +1,29 @@
 from flask import (
     Flask,
-    render_template
+    Response,
+    request
 )
 from jsons import dumps
 from get_hours_logged import *
 
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__)
 
-# Create a URL route in our application for "/"
-@app.route('/time-logged')
+# return time logged
+@app.route('/time-logged', methods=['GET'])
 def time_logged():
-  entries = fetch_time_entries('573')
+  project_id = request.args.get('project_id')
+  print(project_id)
+  entries = fetch_time_entries(project_id)
   hour_summary = construct_hour_summary(entries)
-  return dumps(hour_summary)
 
-# Create a URL route in our application for "/"
-@app.route('/projects')
+  return Response(dumps(hour_summary), mimetype='application/json')
+
+# return project details
+@app.route('/projects', methods=['GET'])
 def projects():
   all_projects = fetch_project_details()
-  return dumps(all_projects)
+  return Response(dumps(all_projects), mimetype='application/json')
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
